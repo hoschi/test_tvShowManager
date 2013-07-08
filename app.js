9aff9d369b9453a17b50abf44839ffcdfea42105
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-08 21:03:33 +0200

    fixup

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-var trakt, argv;
-
-argv = require('optimist').argv;
-trakt = require('api/services/trakt');
-
-// set stuff from command line
-trakt.userName = argv.userName;
-trakt.password = argv.password;
-
 // Start sails and pass it command line arguments
-require('sails').lift(argv);
+require('sails').lift(require('optimist').argv);
