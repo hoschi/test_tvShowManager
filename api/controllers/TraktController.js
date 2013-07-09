9bcc8ef14d02b2b7ca556a8eba4a83654a82c393
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-09 22:32:36 +0200

    try to use trakt npm package to fetch data

--- api/controllers/TraktController.js
+++ api/controllers/TraktController.js
@@ -3,12 +3,32 @@
 	-> controller
 ---------------------*/
 var trakt = require('../services/trakt');
+
+// setup trakt module
+// TODO refactore this and getFirst of SettingsController into model?!
+Settings.find(1).done(function(err, settings){
+	if (err) {
+		console.error(err);
+	}
+	if (!settings) {
+		console.error("No settings found");
+
+	}
+
+	console.log("setup trakt service");
+	trakt.settings = settings.values;
+});
+
 var TraktController = {
 
 	// To trigger this action locally, visit: `http://localhost:port/trakt/collection`
 	collection: function (req,res) {
-		res.json([trakt.userName, trakt.password]);
-
+		trakt.getAllShows(function (err, shows) {
+			if (err) {
+				return res.send(err, 500);
+			}
+			res.json(shows);
+		});
 	}
 
 };
--- api/services/trakt.js
+++ api/services/trakt.js
@@ -1,7 +1,32 @@
-var trakt;
+var trakt, TraktClient;
 
-trakt = {};
+TraktClient = require('trakt');
 
-trakt.test = "foo test";
+trakt = {
+	settings:{
+		traktUsername:"",
+		traktPassword:"",
+		traktApiKey:""
+	}
+};
+
+trakt.getOrCreateClient = function () {
+	if (!this.client) {
+		this.client = new TraktClient({
+			'username':this.settings.traktUsername,
+			'password':this.settings.traktPassword,
+			'api_key':this.settings.traktApiKey
+		});
+		console.log("trakt client created with settings", this.settings);
+	}
+	return this.client;
+};
+
+trakt.getAllShows = function (callback) {
+	var client;
+
+	client = this.getOrCreateClient();
+	return client.request('user', 'lastaciiitivity', {}, callback);
+};
 
 module.exports = trakt;
