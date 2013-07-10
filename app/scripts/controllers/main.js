fec27af35e2b39f1f6b3825d6e3e2600fbac035b
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-10 10:37:54 +0200

    add simple show template

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,11 +3,12 @@
 (function() {
 	var app = angular.module('tvShowManagerApp');
 	app.controller('MainCtrl', function ($scope, Restangular) {
-		$scope.apiKey = 'dfb795ca8c3d1c8e3e432cc54e567deb';
-		$scope.userName = 'hoschi';
-		$scope.passwordSha = '36a8381f81d956f8bba27ed600c0c9ead4b8cbc4';
-		Restangular.all('awesomethings').getList().then(function(things){
-			$scope.awesomeThings = things;
+		var trakt;
+
+		trakt = Restangular.all('trakt');
+
+		trakt.customGET('collection').then(function(shows){
+			$scope.shows = shows;
 		},function(err){
 			console.error(err);
 		});
