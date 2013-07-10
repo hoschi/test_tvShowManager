c4ce30067df85c2fe1816845b5659fcd9f08f69a
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-10 13:26:08 +0200

    add simple error handling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,17 @@
 	app.controller('MainCtrl', function ($scope, Restangular) {
 		var trakt;
 
+		// init
+		$scope.errors = [];
+
+		// get shows
 		trakt = Restangular.all('trakt');
 
 		trakt.customGET('collection').then(function(shows){
 			$scope.shows = shows;
 		},function(err){
 			$scope.shows = undefined;
+			$scope.errors.push("Can't fetch shows: " + err.data);
 			console.error(err);
 		});
 	});
