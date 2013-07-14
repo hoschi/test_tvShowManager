6f140c0a43f815ba5f373962fee8d622660297b7
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-14 16:20:47 +0200

    fixup

--- app/scripts/filters/filterShows.js
+++ app/scripts/filters/filterShows.js
@@ -24,10 +24,12 @@
 					filterState.hideEmpty === false) &&
 
 					((filterState.completelyWatched && show.completelyWatched === filterState.completelyWatched) ||
-					(filterState.watched && show.watched === filterState.watched && show.completelyWatched === false)) &&
+					(filterState.watched && show.watched === filterState.watched && show.completelyWatched === false) ||
+					(filterState.watched === false && filterState.completelyWatched === false)) &&
 
 					((filterState.completelyCollected && show.completelyCollected === filterState.completelyCollected) ||
-					(filterState.collected && show.collected === filterState.collected && show.completelyCollected === false));
+					(filterState.collected && show.collected === filterState.collected && show.completelyCollected === false) ||
+					(filterState.collected === false && filterState.completelyCollected === false));
 			});
 		};
 	});
