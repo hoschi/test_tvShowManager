6b8df142d44e354effd59d1b93234d6fb513cde7
Stefan Gojan <vollekannehoschi@gmail.com>
2013-07-20 15:52:34 +0200

    fixed bootstrap settings stuff

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -4,46 +4,28 @@
 // Make sure you call cb() when you're finished.
 module.exports.bootstrap = function (cb) {
 
-/*
- *    AwesomeThing.findAll().done(function(err, things) {
- *        if (err) return cb(err);
- *        Settings.findAll().done(function (err, settingsList) {
- *            if (err) return cb(err);
- *            // ATTENTION doesn't work
- *            if (!things && things.length > 0 &&
- *                settingsList && settingsList.length > 0) {
- *                return cb();
- *            }
- *
- *            AwesomeThing.create({
- *                name: 'AngularJS'
- *            },function() {
- *                AwesomeThing.create({
- *                    name: 'HTML5 Boilerplate'
- *                }, function() {
- *                    AwesomeThing.create({
- *                        name: 'Karma'
- *                    }, function() {
- *                        AwesomeThing.create({
- *                            name: 'SailsJS'
- *                        }, function() {
- *                            AwesomeThing.create({
- *                                name: 'Bootstrap'
- *                            }, function () {
- *                                Settings.create({
- *                                    traktUsername:"",
- *                                    traktPassword:"",
- *                                    traktApiKey:""
- *                                }, cb);
- *                            });
- *                        });
- *                    });
- *                });
- *            });
- *        });
- *    });
- */
-	cb();
+	console.log("running bootstrap");
+	Settings.findAll().done(function (err, settingsList) {
+		if (err) return cb(err);
+
+		// ATTENTION doesn't work
+		if (settingsList && settingsList.length > 0) {
+			console.log("found settings");
+			return cb();
+		}
+
+		console.log("settings not found, creating");
+		Settings.create({
+			traktUsername:"",
+			traktPassword:"",
+			traktApiKey:""
+		}, function (err) {
+			if (err) return callback(err);
+
+			cb("restart please!");
+		});
+	});
+	//cb();
 
 
 };
